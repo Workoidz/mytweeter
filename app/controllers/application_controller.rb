7e6be7e7e47b2163c75c72c18c147bd698ec448b
Ritesh Kapoor <ritesh@workoidz.com>
2020-04-18 19:49:10 +0530

    Security glitch resolved, Bcrypt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+	#protect_from_forgery with: :expection
+	before_action :ensure_login
+
+	protected
+
+		def ensure_login
+			redirect_to login_path unless session[:user_id]
+			
+		end
+
 end
